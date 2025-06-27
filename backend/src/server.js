e8795ef4993378f2a31f62c4106e359496b6e6f1
stringnet <135693628+stringnet@users.noreply.github.com>
2025-06-27 02:51:42 -0500

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,22 +1,27 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
-const apiRoutes = require('./api/routes');
+const apiRoutes = require('./api/routes'); // Importamos nuestras rutas
 
 const app = express();
 const server = http.createServer(app);
 
-// Middlewares
-app.use(cors({
-  origin: [process.env.FRONTEND_URL, process.env.ADMIN_URL],
-}));
+// Configuración de CORS para permitir peticiones desde nuestros dominios de frontend
+const corsOptions = {
+  origin: [process.env.FRONTEND_URL, process.env.ADMIN_URL]
+};
+
+app.use(cors(corsOptions));
+
+// Middleware para que Express entienda peticiones con cuerpo en formato JSON
 app.use(express.json());
 
-// API Routes
-app.use('/api', apiRoutes);
-
+// Ruta principal de prueba
 app.get('/', (req, res) => {
   res.send('El servidor del Crack de las Jugadas está operativo!');
 });
+
+// Usamos nuestro enrutador para todas las peticiones que empiecen con /api
+app.use('/api', apiRoutes);
 
 module.exports = { app, server };
